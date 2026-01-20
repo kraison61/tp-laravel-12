facaaca1f542e1007cce0880c175c8148adaf2a4
kraison <oun.cav@gmail.com>
2026-01-20 17:01:22 +0700

    20260120

--- resources/views/layouts/partials/_items.blade.php
+++ resources/views/layouts/partials/_items.blade.php
@@ -1,6 +1,6 @@
 @foreach ($images as $image)
-    <div data-sort="1" class="maso-item col-md-4 cat1 cat2 cat4">
-        <a class="img-box lightbox"
+    <div data-sort="1" class="maso-item col-md-4">
+        <a class="img-box"
            href="{{ asset($image->img_url) }}"
            data-lightbox-anima="fade-top">
             <img src="{{ asset($image->img_url) }}"
