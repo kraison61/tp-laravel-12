2bd8f3588218b0af999ae9a7def0c62948e06a88
kraison61 <oun.cav@gmail.com>
2026-02-19 21:47:28 +0700

    Error fixed

--- resources/views/pages/service.blade.php
+++ resources/views/pages/service.blade.php
@@ -1,86 +1,64 @@
 @extends('layouts.app')
 
-@section('title',$service->services->first()->title)
-@section('description',trim($service->services->first()->description))
-@section('image',$service->services->first()->img_1)
+@php
+    // 1. ดึงข้อมูลตัวแรกมาเก็บไว้ก่อน เพื่อป้องกัน Error และลดการเขียนซ้ำ
+    $item = $service->services->first();
+    
+    // 2. เตรียมข้อมูลสำหรับ JSON-LD ในรูปแบบ Array (แก้ปัญหา Syntax Error จากเครื่องหมาย @)
+    $schemaData = [
+        "@context" => "https://schema.org",
+        "@graph" => [
+            [
+                "@type" => "ConstructionBusiness",
+                "@id" => url('/') . "#organization",
+                "name" => "บริษัทธีรพงษ์เซอร์วิส จำกัด",
+                "url" => url('/'),
+                "logo" => [
+                    "@type" => "ImageObject",
+                    "url" => asset('storage/images/logo.png')
+                ],
+                "image" => asset('storage/images/img_landing_1.png'),
+                "telephone" => "+66627188847",
+                "priceRange" => "$$",
+                "address" => [
+                    "@type" => "PostalAddress",
+                    "streetAddress" => "14 ต.บางกร่าง อ.บางกรวย",
+                    "addressLocality" => "Nonthaburi",
+                    "postalCode" => "11000",
+                    "addressCountry" => "TH"
+                ]
+            ],
+            [
+                "@type" => "WebPage",
+                "@id" => url()->current() . "#webpage",
+                "url" => url()->current(),
+                "name" => $item->title ?? 'Service',
+                "inLanguage" => "th"
+            ],
+            [
+                "@type" => "Article",
+                "headline" => $item->h1 ?? ($item->title ?? ''),
+                "description" => trim($item->description ?? ''),
+                "image" => asset('storage/' . ($item->img_1 ?? '')),
+                "author" => [
+                    "@type" => "Person",
+                    "name" => "ช่างรัก (Mr.Theeraphong Sarsuk)"
+                ]
+            ]
+        ]
+    ];
+@endphp
+
+{{-- ส่งข้อมูลไปที่ Meta Tags ใน Layout --}}
+@section('title', $item->title ?? 'Service')
+@section('description', trim($item->description ?? ''))
+@section('image', asset('storage/' . ($item->img_1 ?? '')))
 
 @push('seo-schema')
-<script type="application/ld+json">
-    {
-        "@context": "https://schema.org",
-        "@graph": [{
-                "@type": "ConstructionBusiness",
-                "@id": "{{ url('/') }}#organization",
-                "name": "บริษัทธีรพงษ์เซอร์วิส จำกัด",
-                "url": "{{ url('/') }}",
-                "logo": {
-                    "@type": "ImageObject",
-                    "url": "{{ asset('storage/images/logo.png') }}"
-                },
-                "image": "{{ asset('storage/images/img_landing_1.png') }}",
-                "telephone": "+66627188847",
-                "priceRange": "$$",
-                "address": {
-                    "@type": "PostalAddress",
-                    "streetAddress": "14 ต.บางกร่าง อ.บางกรวย",
-                    "addressLocality": "Nonthaburi",
-                    "postalCode": "11000",
-                    "addressCountry": "TH"
-                }
-            },
-            {
-                "@type": "WebSite",
-                "@id": "{{ url('/') }}#website",
-                "url": "{{ url('/') }}",
-                "name": "Theeraphong Construction",
-                "publisher": {
-                    "@id": "{{ url('/') }}#organization"
-                },
-                "inLanguage": "th"
-            },
-            {
-                "@type": "WebPage",
-                "@id": "{{ url()->current() }}#webpage",
-                "url": "{{ url()->current() }}",
-                "name": "{{ $item->title }}",
-                "isPartOf": {
-                    "@id": "{{ url('/') }}#website"
-                },
-                "inLanguage": "th"
-            },
-            {
-                "@type": "Person",
-                "@id": "{{ url('/') }}#person",
-                "name": "ช่างรัก (Mr.Theeraphong Sarsuk)",
-                "url": "{{ url('/') }}",
-                "jobTitle": "ผู้เชี่ยวชาญด้านงานก่อสร้าง",
-                "worksFor": {
-                    "@id": "{{ url('/') }}#organization"
-                }
-            },
-            {
-                "@type": "Article",
-                "@id": "{{ url()->current() }}#article",
-                "headline": "{{ $item->h1 }}",
-                "description": "{{ trim($item->description) }}",
-                "image": "{{ asset('storage/' . $item->img_1) }}",
-                "author": {
-                    "@id": "{{ url('/') }}#person"
-                },
-                "publisher": {
-                    "@id": "{{ url('/') }}#organization"
-                },
-                "isPartOf": {
-                    "@id": "{{ url()->current() }}#webpage"
-                },
-                "mainEntityOfPage": {
-                    "@id": "{{ url()->current() }}#webpage"
-                },
-                "inLanguage": "th"
-            }
-        ]
-    }
-</script>
+    {{-- พิมพ์ JSON ออกมาโดยใช้ PHP เพื่อเลี่ยงปัญหา Blade Syntax --}}
+    <script type="application/ld+json">
+        {!! json_encode($schemaData, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) !!}
+    </script>
 @endpush
 
 @section('content')
@@ -89,8 +67,8 @@
         <div class="row">
             <div class="col-md-9">
                 <div class="title-base text-left">
-                    <h1>{{ $service->services->first()->title }}</h1>
-                    <p>{{ $service->services->first()->description }}</p>
+                    <h1>{{ $item->title }}</h1>
+                    <p>{{ $item->description }}</p>
                 </div>
             </div>
             <x-breadcrumb>
@@ -99,47 +77,42 @@
         </div>
     </div>
 </div>
+
 <div class="section-empty section-item">
     <div class="container content">
         <div class="row vertical-row">
             <div class="col-md-5">
-                <!-- <img src="../images/mk-8.png" alt="" /> -->
-                {{-- <img class="rounded" src="{{ asset('storage/'.$service->services->first()->img_1) }}" alt="" /> --}}
-                <img class="rounded" src="{{ Storage::url($service->services->first()->img_1) }}" alt="" />
+                <img class="rounded" src="{{ Storage::url($item->img_1) }}" alt="{{ $item->top_alt ?? '' }}" />
             </div>
             <div class="col-md-7">
                 <div class="title-base text-left">
                     <hr />
-                    <h2>{{ $service->services->first()->top_1 }}</h2>
+                    <h2>{{ $item->top_1 }}</h2>
                     <p>Super solutions</p>
                 </div>
-
-                {!! $service->services->first()->content_1 !!}
-
+                {!! $item->content_1 !!}
                 <hr class="space s" />
-
             </div>
         </div>
+
         <hr class="space" />
+
         <div class="row vertical-row" data-anima="fade-bottom">
             <div class="col-md-7">
                 <div class="title-base text-left">
                     <hr />
-                    <h2>{{ $service->services->first()->top_2 }}</h2>
+                    <h2>{{ $item->top_2 }}</h2>
                     <p>Cheap prices</p>
                 </div>
-
-                {!! $service->services->first()->content_2 !!}
-
+                {!! $item->content_2 !!}
                 <hr class="space s" />
             </div>
             <div class="col-md-5 text-right">
-                <!-- <img src="../images/mk-9.png" alt="" /> -->
-                <img src="{{ asset('storage/'.$service->services->first()->img_2) }}" alt="" />
+                <img src="{{ asset('storage/'.$item->img_2) }}" alt="{{ $item->bottom_alt ?? '' }}" />
             </div>
         </div>
+        {{-- ลบ </div> ที่เกินออกให้แล้วครับ --}}
     </div>
-    <hr class="space" />
 </div>
 <i class="scroll-top scroll-top-mobile show fa fa-sort-asc"></i>
 @endsection
